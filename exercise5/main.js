a517cb0622f5df0f629b974a1322f45c8a770faf
Masud Mohamed <masudmohamed2024@gmail.com>
2024-11-20 22:03:12 +0200

    completed exercise 5

---
+++ exercise5/main.js
@@ -0,0 +1,8 @@
+// Create a function named `add` that takes two parameters and returns their sum. Test the function by calling it with different arguments. (use arrow function)
+
+const add = (a,b)=>{
+    return a+b;
+}
+
+let sum = add (9,6);
+console.log ("Sum=" + " " +sum); // output: 15
