f6d28ace49cc1c2b56e611cb96531fd9b5658907
Masud Mohamed <masudmohamed2024@gmail.com>
2024-11-24 22:51:52 +0200

    Added exercise 8

--- exercise6/main.js
+++ exercise6/main.js
@@ -1,4 +1,6 @@
 // Create an array named colors with three colors and log each color to the console.
 
 let colors= ["blue","green","yellow",];
-console.log(colors);
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
---
+++ exercise8/main.js
@@ -0,0 +1,10 @@
+// Create an object named car with properties make, model, and year. Log each property to the console.
+
+let car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2022
+};
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
