6ac6ea3c5868779b8aded9e9466089922cede309
Masud Mohamed <masudmohamed2024@gmail.com>
2024-11-24 22:59:01 +0200

    Added exercise 9

---
+++ exercise9/main.js
@@ -0,0 +1,12 @@
+// Add a method start to the car object that logs "The car has started" to the console and call this method.
+
+let car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2022,
+    start: function(){
+        console.log("The car has started")
+    } 
+}
+
+car.start(); 
