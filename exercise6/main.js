0f4baa9c2943879ff0690b989fc16940678fe827
Masud Mohamed <masudmohamed2024@gmail.com>
2024-11-24 22:37:36 +0200

    Adeed exercise 7

--- exercise6/main.js
+++
@@ -1,6 +0,0 @@
-// Create an array named colors with three colors and log each color to the console.
-
-let colors = ["Blue", "Red", "Green"];
-console.log(colors[0]);
-console.log(colors[1]);
-console.log(colors[2]);
---
+++ exercise7/main.js
@@ -0,0 +1,7 @@
+// Add a color to the end of the colors array, remove the first color, and log the array.
+
+let colors = ["red", "green", "blue"];
+colors.push("yellow");
+console.log(colors);
+colors.shift();
+console.log(colors);
