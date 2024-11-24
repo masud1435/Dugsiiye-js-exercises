45fe3fabace5018ab6e062ae2d6dcb90ecf69449
Masud Mohamed <masudmohamed2024@gmail.com>
2024-11-24 23:02:21 +0200

    Added exercise 10

---
+++ exercise10/main.js
@@ -0,0 +1,18 @@
+// Create an array named library that contains multiple objects. Each object should represent a book with properties title, author, and year. Log the title of the first book and the author of the second book.
+
+const library = [
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    year: 1960
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    year: 1949
+  }
+];
+
+console.log(library[0].title); 
+
+console.log(library[1].author); 
