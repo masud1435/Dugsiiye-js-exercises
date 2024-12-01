f0b8932debba5d83b67efb78aba12f7e763c6e91
Masud Mohamed <masudmohamed2024@gmail.com>
2024-12-01 22:35:44 +0200

    Added exercise 17

---
+++ exercise17/main.js
@@ -0,0 +1,17 @@
+
+
+let tempreture = 14;
+
+if(tempreture < 0) {
+
+    console.log("Very cold.");
+
+}else if(tempreture <=15){
+
+    console.log("Cold.");
+
+}else if (tempreture <=25){
+    console.log("Worm.");
+}else{
+    console.log("Hot.")
+}
