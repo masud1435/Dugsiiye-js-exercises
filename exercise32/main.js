41e6b7b5937527f3e59b2e2bd5bbe3f056d113b5
Masud Mohamed <masudmohamed2024@gmail.com>
2025-02-08 19:24:20 +0200

    Added Exercise 32

---
+++ exercise32/main.js
@@ -0,0 +1,8 @@
+const text = document.querySelectorAll('.text');
+console.log(text);
+
+const header = document.querySelectorAll('.header');
+console.log(header);
+
+const img = document.querySelectorAll('.img');
+console.log(img);
