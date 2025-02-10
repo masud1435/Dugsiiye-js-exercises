500a3147a333ab51479a2d4c9c503a77ddd02ee6
Masud Mohamed <masudmohamed2024@gmail.com>
2025-02-10 22:09:24 +0200

    Added Exercise 33

---
+++ exercise33/main.js
@@ -0,0 +1,10 @@
+const header = document.querySelector('#header');
+function changeText(){
+    header.textContent = 'My name is Masud Mohamed';
+}
+
+const text = document.querySelector('#pragraph');
+function changeElement(){
+    text.innerHTML =  '<strong>After changing </strong>';
+
+};
